2fc86648f7e77781433fb3c67b959373289d4da9
Riya Shah <riyashah614@gmail.com>
2022-10-29 12:49:36 +0530

    First commit

---
+++ src/main/java/com/springrest/springrest/service/CourseServiceImple.java
@@ -0,0 +1,53 @@
+package com.springrest.springrest.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.springrest.springrest.entity.Course;
+
+@Service
+public class CourseServiceImple implements CourseService {
+	
+	List<Course> list;
+	
+
+	public CourseServiceImple() {
+		super();
+		list = new ArrayList<>();
+		list.add(new Course(145, "Java Course", "This Course contains basics of java"));
+		list.add(new Course(146, "Java Advance Course", "This Course contains advance of java"));
+	}
+
+
+	@Override
+	public List<Course> getCourses() {
+		// TODO Auto-generated method stub
+		return list;
+	}
+
+
+	@Override
+	public Course getCourse(long courseId) {
+		// TODO Auto-generated method stub
+		Course c = null;
+		
+		for(Course course: list) {
+			if(course.getId() == courseId) {
+				c=course;
+				break;
+			}
+		}
+		return c;
+	}
+	
+	
+	@Override
+	public Course addCourse(Course course) {
+		// TODO Auto-generated method stub
+		list.add(course);
+		return course;
+	}
+
+}
